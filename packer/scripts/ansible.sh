23fea0ba901b77d05dcf753d001bdb5e17bb46b0
Naveen Suresh <naveensrivatsa@gmail.com>
2022-04-20 13:20:38 +0530

    Initial commit

---
+++ packer/scripts/ansible.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+
+# Install Ansible repository.
+
+sudo yum update -y
+sudo amazon-linux-extras install ansible2 -y
---
+++ packer/scripts/cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+
+# Uninstall Ansible and remove PPA.
+
+sudo yum remove ansible2 -y
+
