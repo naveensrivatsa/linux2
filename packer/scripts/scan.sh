7b71a765378c9f73170fb5961e46b8fcf4dfcfc3
Naveen Suresh <54731216+naveensrivatsa@users.noreply.github.com>
2022-04-23 22:34:25 +0530

    update

--- packer/scripts/scan.sh
+++ packer/scripts/scan.sh
@@ -6,8 +6,8 @@ sudo yum install -y openscap-scanner
 sudo yum install -y scap-security-guide
 mkdir -p /var/tmp/roles/security
 sudo wget --quiet -O /var/tmp/roles/security/Red_Hat_Enterprise_Linux_7.xml https://www.redhat.com/security/data/oval/Red_Hat_Enterprise_Linux_7.xml
-local base_dir="/var/tmp/roles/security"
-local output_dir="$base_dir/output"
+# local base_dir="/var/tmp/roles/security"
+# local output_dir="$base_dir/output"
 mkdir -p $output_dir
 echo "Running PCI compliance scan..."
 sudo oscap xccdf eval --fetch-remote-resources --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf /var/tmp/roles/securityoutput/arf-pci-dss.xml --report $output_dir/report-pci-dss.html /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
