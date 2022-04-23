57c052b7da9535ae6ea5831e7c4cec3adbbbed2c
Naveen Suresh <54731216+naveensrivatsa@users.noreply.github.com>
2022-04-24 00:38:13 +0530

    update

--- packer/scripts/scan.sh
+++ packer/scripts/scan.sh
@@ -10,6 +10,6 @@ sudo wget --quiet -O /var/tmp/roles/security/Red_Hat_Enterprise_Linux_7.xml http
 # local output_dir="$base_dir/output"
 mkdir -p /var/tmp/roles/security/output
 echo "Running PCI compliance scan..."
-sudo oscap xccdf eval --fetch-remote-resources --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf /var/tmp/roles/securityoutput/arf-pci-dss.xml --report /var/tmp/roles/security/report-pci-dss.html /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+sudo oscap xccdf eval --fetch-remote-resources --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf /var/tmp/roles/security/output/arf-pci-dss.xml --report /var/tmp/roles/security/report-pci-dss.html /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
 echo "Running DISA STIG compliance scan..."
 sudo oscap xccdf eval --fetch-remote-resources --profile xccdf_org.ssgproject.content_profile_stig-rhel7-disa --results-arf /var/tmp/roles/security/output/arf-disa-stig.xml --report /var/tmp/roles/security/report-disa-stig.html /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
